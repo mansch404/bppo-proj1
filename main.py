592dc02c1f12822b6860c9b57970ed6e3b92bd31
manuel-julian <mansch404@gmail.com>
2025-10-30 13:55:54 +0100

    Added BPMN

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     log = pm4py.read_xes('bpi-chall.xes')
     net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(log)
     pm4py.view_petri_net(net, initial_marking, final_marking, format="svg")
+
+    process_tree = pm4py.discover_process_tree_inductive(log)
+    bpmn_model = pm4py.convert_to_bpmn(process_tree)
+    pm4py.view_bpmn(bpmn_model)
